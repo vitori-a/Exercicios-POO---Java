217cdffaf4f6c6563a500c0e69234f6346fce98b
Vitoria Natasha <98421932+vitori-a@users.noreply.github.com>
2022-06-07 10:29:18 -0300

    Add files via upload

---
+++ Exercicios POO - Java/Exercicio3/ProjetoPrincipal.java
@@ -0,0 +1,11 @@
+package Exercicio3;
+
+public class ProjetoPrincipal {
+    public static void main(String[] args) {
+        // Projeto Principal
+        Cachorro c = new Cachorro();
+
+        c.reagir("Olá");
+
+    }
+}
